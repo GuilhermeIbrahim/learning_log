9f669974f5365534052be3ab69d9fb6011768570
GuilhermeIbrahim <guilhermeibrahim08@gmail.com>
2026-04-27 15:00:13 -0300

    Adicionado o topics.html, na urls do app learning_log adicionado o caminho, e na views a funcionalidade, além de correcão na base.html

--- learning_log/views.py
+++ learning_log/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Topic
 
 def index(request):
     """Página principal do Learning_log"""
     return render(request, 'learning_log/index.html')
+
+def topics(request):
+    """Mostra todos os assuntos"""
+    topic = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_log/topics.html', context)
