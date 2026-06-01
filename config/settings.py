8ef73bd65312ec4b148fadb3d4302caade53da75
Ibrahim Guilherme <guilhermeibrahim08@gmail.com>
2026-06-01 14:19:13 -0300

    Modifiicação de email como opcional para none, pois ainda não existe servidor smtp para o envio deste email e esta função gerava erro 500 ao registrar uma conta no site.

--- config/settings.py
+++ config/settings.py
@@ -161,7 +161,7 @@ SITE_ID = 1
 
 # Allauth configuration
 ACCOUNT_SIGNUP_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = 'optional'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 ACCOUNT_LOGIN_METHODS = {'username', 'email'}
 ACCOUNT_SIGNUP_FIELDS = ['email*', 'username*', 'password1*', 'password2*']
 SOCIALACCOUNT_AUTO_SIGNUP = True
