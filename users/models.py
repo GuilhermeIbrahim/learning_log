8330ea9d1ff3e19b47e98ec1bfad3d2507d3fcc4
GuilhermeIbrahim <guilhermeibrahim08@gmail.com>
2026-06-07 10:07:06 -0300

    Adição do modelo de UserProfile

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(blank=True, verbose_name='Biografia')
+    campus = models.CharField(max_length=100, blank=True, verbose_name='Campus')
+    matricula = models.CharField(max_length=14, blank=True, verbose_name='Matrícula')
+    tipo_vinculo = models.CharField(max_length=50, blank=True, verbose_name='Tipo de Vínculo')
+    foto_url = models.URLField(blank=True, verbose_name='Foto')
+    
+    def __str__(self):
+        return f'{self.user.username}'
